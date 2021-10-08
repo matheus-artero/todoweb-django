1abcf451373ee998f43f4d2b228854e491550cb5
Matheus Artero <matheus.artero@veigaepostal.com.br>
2021-10-08 10:28:06 -0300

    add models lista e item e apply migrations

--- aplicativo/admin.py
+++ aplicativo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Lista, Item
 
 # Register your models here.
+admin.site.register(Lista)
+admin.site.register(Item)
--- aplicativo/models.py
+++ aplicativo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Lista(models.Model):
+    name = models.CharField(max_length=25)
+    desc = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"{self.id}, {self.name}"
+
+
+class Item(models.Model):
+    lista = models.ForeignKey(Lista, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=40)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.id}, {self.texto}, {self.status}, {self.lista}"
