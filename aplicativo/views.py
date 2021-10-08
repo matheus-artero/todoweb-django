49684587a3b73be2d0a02f24071a262c73adfe35
Matheus Artero <matheus.artero@veigaepostal.com.br>
2021-10-08 16:57:20 -0300

    mod to show list attributes and list items

--- aplicativo/views.py
+++ aplicativo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from .models import Lista, Item
 
 
 def view_home(request):
-    return render(request, 'aplicativo/index.html')
+    lista = Lista.objects.all().first()
+    items  = Item.objects.filter(lista=lista)
+    return render(request, 'aplicativo/index.html', {'lista': lista, 'items': items})
